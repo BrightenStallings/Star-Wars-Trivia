880f4a2fa44bd9f859070dfed4d9b00ada3fc9d7
Brighten Stallings <239831438+BrightenStallings@users.noreply.github.com>
2026-01-14 18:02:39 -0500

    Add files via upload

---
+++ STAR WARS TRIVIA_2.0.py
@@ -0,0 +1,341 @@
+trivia_sets = {
+
+#"STAR WARS: A new hope" trivia questions
+    "1": {
+
+"title":"STAR WARS: A New Hope",
+"questions" : (
+             "1.) What is the name of the space station that destroyed the planet Alderaan?",
+             "2.) What kind of environment is Luke's home planet Tattoine depicted as?",
+             "3.) What is the name of the person that Han-Solo owes money to in the first movie?",
+             "4.) What is the name of the man that initially taught Luke Skywalker about the force?",
+             "5.) What is the color of the first lightsaber that is shown on screen?",
+             "6.) How many parsecs did Han-Solo claim he made the Kessel run in?",
+             "7.) What year was 'Star Wars: A new Hope' released in?",
+             "8.) Who raised Luke Skywalker?",
+             "9.) Who's help does Princess Leia ask for when sending the hologram message?",
+             "10.) Who does Obi-Wan face in a lightsaber battle in this movie?"),
+
+"options" : (
+        ("  a.) The Death Star",      "  b.) Kashyk",         "  c.) Starkiller Base", "  d.) The Star Destroyer"),
+        ("  a.) Oceanic",             "  b.) Forested",       "  c.) Desert",          "  d.) Tundra"),
+        ("  a.) Yoda",                "  b.) Palpatine",      "  c.) Jabba the hut",   "  d.) Jango Fett"),
+        ("  a.) Anakin Skywalker",    "  b.) Obi-Wan Kenobi", "  c.) Qui-Gon Jinn",    "  d.) Mace Windu"),
+        ("  a.) Blue",                "  b.) Green",          "  c.) Red",             "  d.) Yellow"),
+        ("  a.) 2 Parsecs",           "  b.) 25 Parsecs",     "  c.) 8 Parsecs",       "  d.) 12 Parsecs"),
+        ("  a.) 1952",                "  b.) 2002",           "  c.) 1977",            "  d.) 1985"),
+        ("  a.) His Father",          "  b.) His Aunt and Uncle", "  c.) The Jedi Council", "  d.) His Mother"),
+        ("  a.) Obi-Wan Kenobi",      "  b.) Luke Skywalker", "  c.) Han-Solo",        "  d.) R2-D2"),
+        ("  a.) Himself",             "  b.) Luke Skywalker", "  c.) Palpatine",       "  d.) Darth Vader")
+    ),
+
+
+"answers" : ("a", "c", "c", "b", "a", "d", "c", "b", "a", "d")
+
+    },
+
+#"STAR WARS: Empire Strikes Back" trivia questions
+    "2": {
+
+"title":"STAR WARS: Empire Strikes Back",
+"questions" : (
+             "1.) Who is Luke's second teacher of the force?",
+             "2.) What planet did Luke and Yoda train on?",
+             "3.) What is the name of the tundra based planet shown in the beginning of the movie?",
+             "4.) Who said the famous line 'Do or do not. There is no try'?",
+             "5.) What is the name of the creature that attacked Luke at the beginning of the movie?",
+             "6.) Where did the heroes first meet Lando Calrissian?",
+             "7.) What is the name of the bounty hunter that captured Han-Solo?",
+             "8.) What material was Han-Solo encased in?",
+             "9.) Who does Darth vader fight at the end of the movie?",
+             "10.) What injury does Luke Skywalker sustain while fighting Darth Vader?"),
+
+"options" : (
+        ("  a.) Yoda",             "  b.) Obi-Wan Kenobi", "  c.) Darth Vader",    "  d.) Han-Solo"),
+        ("  a.) Tattoine",         "  b.) Hoth",           "  c.) Endor",          "  d.) Dagobah"),
+        ("  a.) Alderon",          "  b.) Hoth",           "  c.) Bespin",         "  d.) Tattoine"),
+        ("  a.) Darth Vader",      "  b.) Princess Leia",  "  c.) Chewbacca",      "  d.) Yoda"),
+        ("  a.) Jabba the hutt",   "  b.) Ewok",           "  c.) Wampa",          "  d.) Tauntaun"),
+        ("  a.) Cloud City",       "  b.) Endor",          "  c.) The Death Star", "  d.) Tattoine"),
+        ("  a.) Jango Fett",       "  b.) Bossk",          "  c.) Cad Bane",       "  d.) Boba Fett"),
+        ("  a.) Ice",              "  b.) Carbonite",      "  c.) Steel",          "  d.) Beskar"),
+        ("  a.) Luke Skywalker",   "  b.) Obi-Wan Kenobi", "  c.) Yoda",           "  d.) Princess Leia"),
+        ("  a.) Light saber burn", "  b.) Cut off leg",    "  c.) Cut off hand",   "  d.) Pulled back muscle")
+    ),
+
+"answers" : ("a", "d", "b", "d", "c", "a", "d", "b", "a", "c")
+
+    },
+
+#"STAR WARS: Return of the Jedi" trivia questions
+    "3": {
+
+"title":"STAR WARS: Return of the Jedi",
+"questions" : (
+    "1.) What is the color of Luke Skywalkers light saber in this movie?",
+    "2.) What planet is the shield generator for the second Death Star located on?",
+    "3.) Who does Luke Skywalker save from Jabba the Hutts Palace?",
+    "4.) Who kills Jabba the Hutt?",
+    "5.) What is the name of the Emperor’s elite red-armored guards?",
+    "6.) What ultimately destroys the second Death Star?",
+    "7.) Who was the other sith shown besides Darth Vader?",
+    "8.) What species lives on the forest moon of Endor?",
+    "9.) Who are the force ghosts that are shown at the end of the movie?",
+    "10.) What does Darth Vader do at the end of the movie to save his son?"
+),
+
+"options" : (
+        ("  a.) Blue",                              "  b.) Red",                               "  c.) Green",                                 "  d.) Purple"),
+        ("  a.) Tatooine",                          "  b.) Endor",                             "  c.) Yavin 4",                               "  d.) Dantooine"),
+        ("  a.) Han-Solo",                          "  b.) Lando Calrissian",                  "  c.) Leia Organa",                           "  d.) C-3PO"),
+        ("  a.) Princess Leia",                     "  b.) Luke Skywalker",                    "  c.) Han Solo",                              "  d.) Lando Calrissian"),
+        ("  a.) Shadow Troopers",                   "  b.) Imperial Royal Guard",              "  c.) Crimson Battalion",                     "  d.) Death Watch"),
+        ("  a.) Ahsoka Tano",                       "  b.) A malfunction in the shield",       "  c.) Millennium Falcon and Rebel pilots",    "  d.) Luke’s Force attack"),
+        ("  a.) Darth Maul",                        "  b.) Darth Malgus",                      "  c.) Darth Tyranus",                         "  d.) Darth Sidious"),
+        ("  a.) Jawas",                              "  b.) Ewoks",                              "  c.) Tusken Raiders",                        "  d.) Wookiees"),
+        ("  a.) Qui-Gon, Mace Windu, and Yoda",     "  b.) Yoda and Obi-Wan",                  "  c.) Obi-Wan and Qui-Gon",                   "  d.) Anakin, Obi-Wan, and Yoda"),
+        ("  a.) Call Han-Solo and Leia for help",   "  b.) Bring Luke back to life",           "  c.) Kill Palpatine",                        "  d.) Send Luke away in a ship")
+    ),
+
+
+"answers" : ("c", "b", "a", "a", "b", "c", "d", "b", "d", "c")
+    },
+
+#"STAR WARS: The Phantom Menace" trivia questions
+    "4":{
+
+"title":"STAR WARS: The Phantom Menace",
+"questions" : (
+    "1.) Who discovers Anakin Skywalker on Tatooine?",
+    "2.) What type of alien species is Jar Jar Binks?",
+    "3.) Who is the ruler of Naboo during the events of The Phantom Menace?",
+    "4.) What is the name of the Sith apprentice revealed in this movie?",
+    "5.) Who has a midichlorian count higher than Yoda?",
+    "6.) What weapon does the Trade Federation use to invade Naboo?",
+    "7.) Who is Anakin’s podracing rival?",
+    "8.) What is the name that Darth Sidious is posing under?",
+    "9.) Who is the first to die in the battle between Darth Maul, Qui-Gon, and Obi-Wan?",
+    "10.) How many blades does Darth Maul’s lightsaber have?"
+),
+
+"options" : (
+        ("  a.) Qui-Gon Jinn",      "  b.) Obi-Wan Kenobi",  "  c.) Mace Windu",      "  d.) Yoda"),
+        ("  a.) Twi'lek",           "  b.) Gungan",          "  c.) Zabrak",          "  d.) Wookiee"),
+        ("  a.) Senator Palpatine", "  b.) Bail Organa",     "  c.) Queen Amidala",   "  d.) Mon Mothma"),
+        ("  a.) Count Dooku",       "  b.) Darth Maul",      "  c.) Darth Bane",      "  d.) General Grievous"),
+        ("  a.) Anakin",            "  b.) Obi-Wan",         "  c.) Qui-Gon",         "  d.) Darth Maul"),
+        ("  a.) Stormtroopers",     "  b.) Battle Droids",   "  c.) Clone Troopers",  "  d.) Mandalorians"),
+        ("  a.) Jabba the Hutt",    "  b.) Sebulba",         "  c.) Kitster",         "  d.) Watto"),
+        ("  a.) Count Dooku",       "  b.) Luke",            "  c.) Palpatine",       "  d.) Owen"),
+        ("  a.) Darth Maul",        "  b.) Anakin",          "  c.) Obi-Wan",         "  d.) Qui-Gon"),
+        ("  a.) One",               "  b.) Two",             "  c.) Three",           "  d.) Four")
+    ),
+
+"answers" : ("a", "b", "c", "b", "a", "b", "b", "c", "d", "b")
+
+    },
+
+#"STAR WARS: Attack of the Clones" trivia questions
+    "5":{
+
+"title":"STAR WARS: Attack of the Clones",
+"questions" : (
+    "1.) What planet does Obi-Wan travel to while investigating the clone army?",
+    "2.) Who orders the assassination of Senator Padmé Amidala at the start of the film?",
+    "3.) What bounty hunter serves as the genetic template for the Clone Army?",
+    "4.) On which planet does Anakin find his mother, Shmi Skywalker?",
+    "5.) Who does anakin confide in after he kills the village of Tusken Raiders?",
+    "6.) Who leads the Separatist Alliance?",
+    "7.) What color is Mace Windu’s lightsaber in this movie?",
+    "8.) Where does the final battle between the Jedi, clones, and droids take place?",
+    "9.) Who kills Jango Fett during the Battle of Geonosis?",
+    "10.) Who secretly marries Anakin Skywalker at the end of the movie?"
+),
+
+
+"options" : (
+    ("  a.) Kamino",           "  b.) Geonosis",       "  c.) Naboo",            "  d.) Tatooine"),
+    ("  a.) Palpatine",        "  b.) Jango Fett",     "  c.) Count Dooku",      "  d.) Watto"),
+    ("  a.) Jango Fett",       "  b.) Boba Fett",      "  c.) Captain Rex",      "  d.) Cad Bane"),
+    ("  a.) Kashyyyk",         "  b.) Geonosis",       "  c.) Tatooine",         "  d.) Jakku"),
+    ("  a.) Yoda",             "  b.) Mace Windu",     "  c.) Obi-Wan",          "  d.) Padmé"),
+    ("  a.) Darth Sidious",    "  b.) Count Dooku",    "  c.) General Grievous", "  d.) Nute Gunray"),
+    ("  a.) Blue",             "  b.) Green",          "  c.) Red",              "  d.) Purple"),
+    ("  a.) Naboo",            "  b.) Geonosis",       "  c.) Coruscant",        "  d.) Mustafar"),
+    ("  a.) Anakin Skywalker", "  b.) Obi-Wan Kenobi", "  c.) Mace Windu",       "  d.) Yoda"),
+    ("  a.) Padmé Amidala",    "  b.) Leia Organa",    "  c.) Ahsoka Tano",      "  d.) Barriss Offee")
+),
+
+
+"answers" : ("a", "a", "a", "c", "d", "b", "d", "b", "c", "a")
+
+    },
+
+#"STAR WARS: Revenge of the Sith" trivia questions
+    "6": {
+
+"title":"STAR WARS: Revenge of the Sith",
+"questions" : (
+    "1.) Which planet does the opening space battle take place above?",
+    "2.) Who kills Count Dooku early in the film?",
+    "3.) What does Mace Windu NOT grant to Anakin?",
+    "4.) Who reveals to Anakin the Sith legend of Darth Plagueis?",
+    "5.) Who gave Anakin the final go-ahead to kill Count Dooku?",
+    "6.) What order was given instructing all clones to turn on the Jedi?",
+    "7.) Who duels General Grievous on Utapau?",
+    "8.) What is the name given to Anakin by Palpatine after killing Mace Windu?",
+    "9.) Where does Obi-Wan defeat Anakin in their final duel?",
+    "10.) How many duals did Obi-Wan Kenobi win in this movie?"
+),
+
+
+"options" : (
+    ("  a.) Coruscant",         "  b.) Mustafar",          "  c.) Naboo",            "  d.) Kashyyyk"),
+    ("  a.) Mace Windu",        "  b.) Anakin Skywalker",  "  c.) Obi-Wan Kenobi",   "  d.) Palpatine"),
+    ("  a.) Chancellor’s Aide", "  b.) Ambassador",        "  c.) Jedi Master",      "  d.) Jedi Knight"),
+    ("  a.) Mace Windu",        "  b.) Palpatine",         "  c.) Obi-Wan Kenobi",   "  d.) Count Dooku"),
+    ("  a.) Obi-Wan",           "  b.) Palpatine",         "  c.) No one",           "  d.) General Grievous"),
+    ("  a.) Order 66",          "  b.) Order 43.1",        "  c.) Order 99",         "  d.) Kill them all"),
+    ("  a.) Yoda",              "  b.) Mace Windu",        "  c.) Obi-Wan Kenobi",   "  d.) Ki-Adi-Mundi"),
+    ("  a.) Darth Tyranus",     "  b.) Darth Maul",        "  c.) Darth Sidious",    "  d.) Darth Vader"),
+    ("  a.) Coruscant",         "  b.) Naboo",             "  c.) Mustafar",         "  d.) Utapau"),
+    ("  a.) 2",                 "  b.) 4",                 "  c.) 1",                "  d.) 3")
+),
+
+
+"answers" : ("a", "b", "c", "b", "b", "a", "c", "d", "c", "a")
+
+    }
+}
+
+
+
+#Printing menu
+def print_menu(trivia_sets):
+    print("\nChoose the movie that you'd like to quiz your self on, or check your highscores!")
+    for key, data in trivia_sets.items():
+        print(f"\n{key}.) {data['title']}")
+    print("\n7.) Show Highscores")
+
+
+#Selecting movie
+def select_movie(trivia_sets):
+    while True:
+        choice = input("\nEnter your choice:").strip()
+        if choice in trivia_sets:
+            return choice
+        elif choice == "7":
+            show_highscore()
+            continue
+
+        print("\nInvalid choice, please try again.")
+
+
+
+
+#Saving highscore
+def save_highscore(movie_title, score):
+    with open("highscores.txt", "a") as file:
+         file.write(f"{movie_title}: {score}%\n")
+
+
+
+#Showing highscore
+def show_highscore():
+    try:
+        with open("highscores.txt", "r") as file:
+            print(" ---------------------------")
+            print(" |       HIGH SCORE        |")
+            print(" ---------------------------")
+            print(file.read())
+    except FileNotFoundError:
+        print("\nNo highscores recorded.")
+
+
+
+
+#Running quiz
+def run_quiz(trivia):
+    score = 0
+    movie_title = trivia["title"]
+    question_num = 0
+    questions = trivia["questions"]
+    options = trivia["options"]
+    answers = trivia["answers"]
+
+    print(f"\n{movie_title}")
+    for question in questions:
+        print("------------------------------------------------------------------------------")
+        print(question)
+        for option in options[question_num]:
+            print(option)
+
+        while True:
+            guess = input("\nEnter your answer:").strip().lower()
+            if guess in ('a', 'b', 'c', 'd'):
+                break
+            print("\nInvalid choice, please try again.")
+
+        if guess == answers[question_num]:
+            score += 1
+            print(f"Correct! Your current score is {score}.")
+        else:
+            print("Incorrect!")
+
+        question_num += 1
+
+    final_score = (score / len(questions) * 100)
+    print("\n")
+    print(" ---------------------------")
+    print(" |      FINAL RESULTS      |")
+    print(" ---------------------------")
+
+    if final_score == 100:
+        print(f"\nYour final score is {final_score}%! You passed with an A+!")
+    elif final_score >= 90:
+        print(f"\nYour final score is {final_score}%! You passed with an A!")
+    elif final_score >= 80:
+        print(f"\nYour final score is {final_score}%! You passed with a B!")
+    elif final_score >= 70:
+        print(f"\nYour final score is {final_score}%! You passed with a C!")
+    elif final_score >= 60:
+        print(f"\nYour final score is {final_score}%! You just BARELY passed with a D! That was a close one!")
+    else:
+        print(f"\nYour final score is {final_score}%! You failed... Try again young padawan.")
+
+    save_highscore(movie_title, final_score)
+
+
+
+#Play again
+def play_again():
+    while True:
+        answer = input("\nWould you like to test your knowledge on a different movie, or check your highscores? (yes/no):").strip().lower()
+        if answer == "yes":
+            return True
+        elif answer == "no":
+            return False
+        else:
+            print("\nInvalid choice, please try again.")
+
+
+
+
+
+def main(trivia_sets):
+    print("______________________________")
+    print("    STAR WARS TRIVIA NIGHT    ")
+    print("______________________________")
+
+    while True:
+        print_menu(trivia_sets)
+        choice = select_movie(trivia_sets)
+        run_quiz(trivia_sets[choice])
+
+        if not play_again():
+            print("\nMay the Force be with you.")
+            break
+
+
+if __name__ == "__main__":
+    main(trivia_sets)
